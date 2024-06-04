160b96277fea128fe4ee8697b61aab78e0688dd5
Márcia Bigão <mbigaod@gmail.com>
2024-06-04 10:49:42 -0300

    feat: adição da função de login do usuário

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -5,3 +5,29 @@ import { compare } from "bcrypt";
 import statusCodes from "../../utils/constants/statusCodes";
 import { User } from "@prisma/client";
 import { sign, verify } from "jsonwebtoken";
+
+export async function login(req: Request, res: Response, next: NextFunction) {
+    try {
+        
+        const user = await prisma.user.findUnique({
+            where: {
+                email: req.body.email
+            }
+        });
+
+        if(!user) {
+            throw new PermissionError("Email e/ou senha incorretos!");
+        }
+
+        const match = compare(req.body.password, user.password);
+
+        if(!match){
+            throw new PermissionError("Email e/ou senha incorretos!");
+        }
+
+        res.status(statusCodes.NO_CONTENT).json("Login realizado com sucesso!");
+
+    } catch (error) {
+        next(error);
+    }
+}
