008b90b82021fd97f3bcb0162f9fb8a61ede9170
Márcia Bigão <mbigaod@gmail.com>
2024-06-04 11:16:07 -0300

    feat: adição de função para gerar o JWT

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -5,6 +5,23 @@ import { compare } from "bcrypt";
 import statusCodes from "../../utils/constants/statusCodes";
 import { User } from "@prisma/client";
 import { sign, verify } from "jsonwebtoken";
+import cookieParser from "cookie-parser";
+
+function generateJWT(user: User, res: Response){
+    const body = {
+        id: user.id,
+        email: user.email,
+        role: user.role,
+        name: user.name,
+    };
+
+    const token = sign({user: body}, process.env.SECRET_KEY || "", {expiresIn: process.env.JWT_EXPIRATION});
+
+    res.cookie("jwt", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development",
+    });
+}
 
 export async function login(req: Request, res: Response, next: NextFunction) {
     try {
@@ -25,9 +42,13 @@ export async function login(req: Request, res: Response, next: NextFunction) {
             throw new PermissionError("Email e/ou senha incorretos!");
         }
 
+        generateJWT(user, res);
+
         res.status(statusCodes.NO_CONTENT).json("Login realizado com sucesso!");
 
     } catch (error) {
+
         next(error);
+
     }
 }
