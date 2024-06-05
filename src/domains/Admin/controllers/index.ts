a0c1a6de3df37a297462d154fff2a1d1292193e6
planetsview <167258518+iMandy10@users.noreply.github.com>
2024-06-05 09:29:11 -0300

    feat: cria AdminRouter

---
+++ src/domains/Admin/controllers/index.ts
@@ -0,0 +1,27 @@
+import { Router, Request, Response, NextFunction } from "express";
+import UserService from "../../User/service/UserService";
+import ArtistService from "../../Artist/service/ArtistService";
+import MusicService from "../../Music/service/MusicService";
+import { notLoggedIn, login, verifyJWT, logout } from "../../../middlewares/auth";
+import { checkRole } from "../../../middlewares/auth";
+
+const AdminRouter = Router();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default AdminRouter;
