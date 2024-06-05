d2fae72c609584ee5aae8472e7320640923bee21
jpmouraf <joaopfurtado@outlook.com>
2024-06-05 15:06:36 -0300

    feat: adiciona update e delete na rota admin

--- src/domains/Admin/controllers/index.ts
+++ src/domains/Admin/controllers/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Router, Request, Response, NextFunction } from "express";
 import UserService from "../../User/service/UserService";
@@ -7,7 +8,33 @@ import { notLoggedIn, login, verifyJWT, logout } from "../../../middlewares/auth
 import { checkRole } from "../../../middlewares/auth";
 
 const AdminRouter = Router();
+AdminRouter.post("/login", notLoggedIn, login);
+AdminRouter.post("/logout", verifyJWT, logout);
 
+AdminRouter.put("/update/:id", checkRole(["admin"]), verifyJWT,async (req: Request, res: Response, next: NextFunction) => {
+	try {
+        
+		const body = req.body;
+		const user = await UserService.updateUser(Number(req.params.id), body);
+		res.json(user);
+
+	} catch (error) {
+        
+		next(error);
+
+	}
+});
+
+AdminRouter.delete("/unlinkUserMusic/:idUser/:idMusic",checkRole(["admin"]),verifyJWT, async (req: Request, res: Response, next: NextFunction) => {
+	try {
+        
+		const unlink = await UserService.unlinkMusic(Number(req.params.idUser), Number(req.params.idMusic));
+		res.json(unlink);
+
+	} catch (error) {
+	    next(error);
+	}
+});
 
 
 
