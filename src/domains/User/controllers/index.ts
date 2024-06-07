b24e873ab934ffe27518d8a3f9723c1cdbeae281
jpmouraf <joaopfurtado@outlook.com>
2024-06-07 09:31:06 -0300

    Status codes no controller do user

--- src/domains/User/controllers/index.ts
+++ src/domains/User/controllers/index.ts
@@ -4,6 +4,7 @@ import { Router, Request, Response, NextFunction } from "express";
 import UserService from "../service/UserService";
 import { notLoggedIn, login, verifyJWT, logout } from "../../../middlewares/auth";
 import { checkRole } from "../../../middlewares/auth";
+import statusCodes from "../../../../utils/constants/statusCodes";
 
 const UserRouter = Router();
 UserRouter.post("/login", notLoggedIn, login);
@@ -18,7 +19,7 @@ UserRouter.post("/account/create", async (req: Request, res: Response, next: Nex
 
 		const body = req.body;
 		const newUser = await UserService.create(body);
-		res.json(newUser);
+		res.status(statusCodes.CREATED).json(newUser);
 
 	} catch (error) {
 
@@ -31,7 +32,7 @@ UserRouter.get("/account/:id",verifyJWT, checkRole(["admin", "user"]), async (re
 	try {
         
 		const user = await UserService.getUserbyId(Number(req.user.id));
-		res.json(user);
+		res.status(statusCodes.SUCCESS).json(user);
 
 	} catch (error) {
         
@@ -46,7 +47,7 @@ UserRouter.put("/account/update/:id", verifyJWT, checkRole(["admin", "user"]), a
         
 		const body = req.body;
 		const user = await UserService.updateUser(Number(req.user.id), body);
-		res.json(user);
+		res.status(statusCodes.SUCCESS).json(user);
 
 	} catch (error) {
         
@@ -59,7 +60,7 @@ UserRouter.put("/account/password/update/:id", verifyJWT, checkRole(["admin", "u
         
 		const body = req.body;
 		const user = await UserService.updateUserPassword(Number(req.user.id), body);
-		res.json(user);
+		res.status(statusCodes.SUCCESS).json(user);
 
 	} catch (error) {
         
@@ -72,7 +73,7 @@ UserRouter.put("/linkUserMusic/:idUser/:idMusic", verifyJWT, checkRole(["admin",
 	try {
         
 		const link = await UserService.linkMusic(Number(req.params.idUser), Number(req.params.idMusic));
-		res.json(link);
+		res.status(statusCodes.SUCCESS).json(link);
 
 	} catch (error) {
         
@@ -85,7 +86,7 @@ UserRouter.delete("/unlinkUserMusic/:idUser/:idMusic", verifyJWT, checkRole(["ad
 	try {
         
 		const unlink = await UserService.unlinkMusic(Number(req.params.idUser), Number(req.params.idMusic));
-		res.json(unlink);
+		res.status(statusCodes.SUCCESS).json(unlink);
 
 	} catch (error) {
 	    next(error);
@@ -96,7 +97,7 @@ UserRouter.get("/listenedMusics/:idUser",verifyJWT,checkRole(["admin", "user"]),
     try {
 		
 		const listenedMusics = await UserService.listenedMusics(Number(req.params.idUser));
-		res.json(listenedMusics);
+		res.status(statusCodes.SUCCESS).json(listenedMusics);
 	} catch (error) {
 	    next(error);
 	}
@@ -108,7 +109,7 @@ UserRouter.delete("/account/delete/:id", verifyJWT, checkRole(["admin", "user"])
 		const user = await UserService.delete(Number(req.user.id));
 		 res.clearCookie("jwt", { httpOnly: true, 
 			secure: process.env.NODE_ENV !== "development"  });
-		res.json(user);
+		res.status(statusCodes.SUCCESS).json(user);
 
 	} catch (error) {
         
