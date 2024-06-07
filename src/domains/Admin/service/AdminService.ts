da363c2891393b591b5083ccdc1b776c91a8e759
planetsview <167258518+iMandy10@users.noreply.github.com>
2024-06-07 13:22:00 -0300

    fix: resolve constantes de validar email.

--- src/domains/Admin/service/AdminService.ts
+++ src/domains/Admin/service/AdminService.ts
@@ -7,10 +7,6 @@ import { InvalidParamError } from "../../../../errors/InvalidParamError";
 import adminRegex from "../../../../utils/constants/verifyEmail";
 
 class AdminService {
-	async validEmail(email: string) {
-		const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-		return regex.test(email);
-	}
 
 	async encryptPassword(password: string) {
 		const saltRounds = 10;
@@ -50,9 +46,9 @@ class AdminService {
 			throw new QueryError("O id não pode ser modificado!");
 		}
 		if (typeof body.email !== "undefined"){
-			const validation = await this.validEmail(body.email);
-			if (!validation) {
-				throw new InvalidParamError("Email inválido!");}
+			if(!adminRegex.test(body.email)) {
+				throw new QueryError("Formato de email inválido!");
+			}
 		}
 		const updatedAdmin = await prisma.user.update({
 			data: {
@@ -127,10 +123,9 @@ class AdminService {
 		if (body.id !== undefined){
 			throw new QueryError("O id não pode ser modificado!");
 		}
-		const validation = await this.validEmail(body.email);
-		if (!validation) {
-			throw new InvalidParamError("Email inválido!");}
-
+		if(!adminRegex.test(body.email)) {
+			throw new QueryError("Formato de email inválido!");
+		}
 		const encrypted = await this.encryptPassword(body.password);
 		const user = await prisma.user.create({
 			data: {
