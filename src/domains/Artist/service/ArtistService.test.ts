9acf3ef5cb9d544c5201c64f72e2b880df6b04c2
jpmouraf <joaopfurtado@outlook.com>
2024-06-19 15:04:00 -0300

    adiciona os testes dos metodo getArtistbyId e getArtists

--- src/domains/Artist/service/ArtistService.test.ts
+++ src/domains/Artist/service/ArtistService.test.ts
@@ -89,10 +89,61 @@ describe('ArtistService - create', () => {
     });
 
 describe('getArtistbyId' , () => {
+    test('um usuário cadastrado com seu id ==> retorna o usuário', async () => {
+        const artist = {
+            id: 1,
+            name: 'cantor',
+            photo: null,
+            streams: 1000
+        };
 
+        prismaMock.artist.findFirst.mockResolvedValue(artist)
+
+        await expect(ArtistService.getArtistbyId(1)).resolves.toEqual(artist);
+    });
+
+    test('tenta buscar um usuário inexistente ==> gera erro', async () => {
+        const artistId = 1;
+        prismaMock.artist.findFirst.mockResolvedValue(null)
+
+        await expect(ArtistService.getArtistbyId(artistId)).rejects.toThrow(
+            new QueryError("O artista que você está buscando não está cadastrado")
+        );
+
+        expect(prismaMock.artist.findFirst).toHaveBeenCalledWith({where: {id: artistId}})
+    });
+    
 });
 
 describe('getArtists' , () => {
+    test('dois usuários estão cadastrados ==> retorna dois usuários', async () => {
+        const firstUser = {
+            id: 1,
+            name: 'cantor',
+            photo: null,
+            streams: 1000
+        };
+        const secondUser = {
+            id: 2,
+            name: 'vocalista',
+            photo: null,
+            streams: 2000
+        };
+
+        prismaMock.artist.findMany.mockResolvedValue([firstUser, secondUser]);
+
+        const artists = await ArtistService.getArtists();
+
+        expect(artists).toEqual([firstUser, secondUser]);
+    });
+
+    test('nao há nenhum usuário cadastrado ==> gera erro', async () => {
+        prismaMock.artist.findFirst.mockResolvedValue(null);
+
+        await expect(ArtistService.getArtists()).rejects.toThrow(
+            new QueryError("Não há nenhum artista cadastrado.")
+        )
+    });
 
 });
 
